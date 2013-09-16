ea6719a0ad3840189dcdc853740d9c5b37d7d02b
Michael Nason <m.s.nason@gmail.com>
2013-09-16 14:57:27 -0700

    Added newMessage view and template I forgot in last commit; update login template

---
+++ app/scripts/views/newMessage.js
@@ -0,0 +1,18 @@
+/*global bridgelyApp, Backbone, JST*/
+
+bridgelyApp.Views = bridgelyApp.Views || {};
+
+(function () {
+    'use strict';
+
+    bridgelyApp.Views.newMessageView = Backbone.View.extend({
+
+        template: JST['app/scripts/templates/newMessage.ejs'],
+        el: 'form',
+        render: function() {
+          return this.$el.html( this.template );
+        }
+
+    });
+
+})();
