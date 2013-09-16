243d7f2f97281f824dd49846235cba148cf8f4a1
Michael Nason <m.s.nason@gmail.com>
2013-09-16 16:15:49 -0700

    Update employees collection to be pageable, append pagination controls

--- app/scripts/collections/employees.js
+++ app/scripts/collections/employees.js
@@ -5,10 +5,14 @@ bridgelyApp.Collections = bridgelyApp.Collections || {};
 (function () {
     'use strict';
 
-    bridgelyApp.Collections.EmployeesCollection = Backbone.Collection.extend({
+    bridgelyApp.Collections.EmployeesCollection = Backbone.PageableCollection.extend({
 
         model: bridgelyApp.Models.EmployeeModel,
-        url: "http://localhost:3000/v1/employees"
+        url: "http://localhost:3000/v1/employees",
+        state: {
+          pageSize: 3
+        },
+        mode: "client" // page entirely on the client side
 
     });
 
