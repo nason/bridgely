f59258a0cba46696922e145765eef947c982ba4a
Michael Nason <m.s.nason@gmail.com>
2013-09-23 20:25:06 -0700

    Add success modals and logic to new message and question views

--- app/scripts/views/newMessage.js
+++ app/scripts/views/newMessage.js
@@ -10,15 +10,19 @@ bridgelyApp.Views = bridgelyApp.Views || {};
         template: JST['app/scripts/templates/newMessage.ejs'],
         el: $('<form />'),
         render: function() {
+          this.delegateEvents();
           this.$el.html( this.template );
           $('#content').html( this.el );
           return this.el;
         },
         events: {
-          'submit' : 'sendMessage'
+          'submit' : 'sendMessage',
+          'click #success-button' : 'next'
         },
         sendMessage: function(event) {
           event.preventDefault();
+          this.$('button[type=submit]').prop('disabled', true);
+
           $.ajax({
             type: 'POST',
             url: bridgelyApp.apiUrl + '/messages',
@@ -26,18 +30,28 @@ bridgelyApp.Views = bridgelyApp.Views || {};
               message: {
                 body: this.$('textarea').val(),
                 employee_ids: 'all', // TODO: get a list of ids or 'all'
-                company_id: bridgelyApp.session.get('account').company.id
+                company_id: bridgelyApp.session.get('company').id
               }
             },
             success: function() {
-              //TODO: Success screen
-              console.log('message sent successfully')
+              console.log('message sent successfully');
+              $('#successModal').modal({
+                backdrop: 'static',
+                keyboard: false
+              });
             },
             error: function() {
               //TODO: Error screen
               console.log('mesaage send failed!!')
             }
           })
+        },
+        next: function() {
+          this.$('#successModal').on('hidden.bs.modal', function () {
+            bridgelyApp.MessageRouter.navigate('message-history', {trigger: true});
+          })
+
+          this.$('#successModal').modal('hide');
         }
 
     });
--- app/scripts/views/newQuestion.js
+++ app/scripts/views/newQuestion.js
@@ -10,15 +10,51 @@ bridgelyApp.Views = bridgelyApp.Views || {};
         template: JST['app/scripts/templates/newQuestion.ejs'],
         el: $('<form />'),
         render: function() {
+          this.delegateEvents();
           this.$el.html( this.template );
           $('#content').html( this.el );
           return this.el;
         },
         events: {
-          'submit' : 'sendQuestion'
+          'submit' : 'sendQuestion',
+          'click #success-button' : 'next'
         },
-        sendQuestion: function() {
-          // $.ajax()
+        sendQuestion: function(event) {
+          event.preventDefault();
+          this.$('button[type=submit]').prop('disabled', true);
+
+          $.ajax({
+            type: 'POST',
+            url: bridgelyApp.apiUrl + '/questions',
+            data: {
+              question: {
+                title: this.$('#question-title').val(),
+                response_tag: this.$('#question-tag').val(),
+                message: {
+                  body: this.$('textarea').val(),
+                  employee_ids: 'all', // TODO: get a list of ids or 'all'
+                  company_id: bridgelyApp.session.get('company').id
+                }
+              }
+            },
+            success: function() {
+              console.log('question sent successfully')
+              $('#successModal').modal({
+                backdrop: 'static',
+                keyboard: false
+              });
+            },
+            error: function() {
+              //TODO: Error screen
+              console.log('question send failed!!')
+            }
+          })
+        },
+        next: function() {
+          this.$('#successModal').on('hidden.bs.modal', function () {
+            bridgelyApp.MessageRouter.navigate('message-history', {trigger: true});
+          })
+          this.$('#successModal').modal('hide');
         }
 
     });
