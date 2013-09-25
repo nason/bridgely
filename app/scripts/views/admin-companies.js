dfdd6fd812f7b0166c926ca32a42f4aacc6e659a
Michael Nason <m.s.nason@gmail.com>
2013-09-24 20:45:47 -0700

    Admin company view cleanup

--- app/scripts/views/admin-companies.js
+++ app/scripts/views/admin-companies.js
@@ -6,20 +6,17 @@ bridgelyApp.Views = bridgelyApp.Views || {};
     'use strict';
 
     bridgelyApp.Views.AdminCompaniesView = Backbone.View.extend({
-        // collection: bridgelyApp.Collections.AdminCompaniesCollection,
         template: JST['app/scripts/templates/admin-companies.ejs'],
         initialize: function() {
           this.collection = new bridgelyApp.Collections.AdminCompaniesCollection();
-
           this.collection.on('sync', function(collection, resp, options) {
             this.render();
           }, this);
-
           this.collection.fetch();
         },
 
         render: function() {
-          $('content').html( this.$el.html( this.template(this.collection.models) ) );
+          this.$el.html( this.template(this.collection.models) );
           this.delegateEvents();
           return this.el;
         }
