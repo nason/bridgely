5841d9c0ba87d29591dd481bade81612f20254dd
Michael Nason <m.s.nason@gmail.com>
2013-09-24 18:11:17 -0700

    Scaffold admin company model

---
+++ app/scripts/models/admin-company.js
@@ -0,0 +1,12 @@
+/*global bridgelyApp, Backbone*/
+
+bridgelyApp.Models = bridgelyApp.Models || {};
+
+(function () {
+    'use strict';
+
+    bridgelyApp.Models.AdminCompanyModel = Backbone.Model.extend({
+
+    });
+
+})();
