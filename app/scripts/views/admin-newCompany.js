4da9698f885e6f3dff6cead191593a3ff192ea4f
Michael Nason <m.s.nason@gmail.com>
2013-09-24 22:12:01 -0700

    Add an untested company create method

--- app/scripts/views/admin-newCompany.js
+++ app/scripts/views/admin-newCompany.js
@@ -14,7 +14,39 @@ bridgelyApp.Views = bridgelyApp.Views || {};
         },
         createCompany: function(event) {
           event.preventDefault();
-          console.log('yes!');
+          this.$('button[type=submit]').prop('disabled', true);
+
+          $.ajax({
+            type: 'POST',
+            url: bridgelyApp.apiUrl + '/admin/company',
+            headers: {
+              // left this in bc ajaxSetup headers didn't apply here?
+              'Authorization': "Token token="+bridgelyApp.session.get('auth_token')
+            },
+            data: {
+              company: {
+                name: this.$('input[name=name]').val(),
+                settings: {
+                  autoresponder: this.$('textarea[name=autoresponder]').val()
+                },
+                user: {
+                  email: this.$('input[name=email]').val(),
+                  password: his.$('input[name=password]').val()
+                }
+              }
+            },
+            success: function() {
+              // TODO: Flash success?
+              console.log('company created successfully');
+              bridgelyApp.AdminRouter.navigate('admin/companies', {trigger: true});
+            },
+            error: function() {
+              //TODO: Error screen
+              console.log('mesaage send failed!!')
+              bridgelyApp.AdminRouter.navigate('admin/companies', {trigger: true});
+              alert('Oh no, something went wrong. Try again...');
+            }
+          })
         },
         render: function() {
           this.$el.html( this.template ).addClass('form-horizontal').attr('role', 'form');
