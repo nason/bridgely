1639833114e15a08333560b1b1e2c0f1983fbc73
Michael Nason <m.s.nason@gmail.com>
2013-09-29 15:21:02 -0700

    Add question view and template

--- app/scripts/views/question.js
+++ app/scripts/views/question.js
@@ -8,7 +8,29 @@ bridgelyApp.Views = bridgelyApp.Views || {};
     bridgelyApp.Views.QuestionView = Backbone.View.extend({
         model: bridgelyApp.Models.QuestionView,
         template: JST['app/scripts/templates/question.ejs'],
-        events: {},
+        events: {
+          'click .back' : 'back',
+          'click .employee' : 'viewEmployee'
+        },
+        back: function() {
+          event.preventDefault();
+          bridgelyApp.MessageRouter.navigate('message-history', {trigger: true});
+        },
+        viewEmployee: function() {
+          event.preventDefault;
+          var employeeId = event.target.className.split('-')[1];
+          bridgelyApp.EmployeeRouter.navigate('employee/' + employeeId, {trigger: true});
+        },
+        getEmployees: function() {
+          var question = this;
+          $.ajax({
+            method: 'GET',
+            url: this.model.url() + '/employees',
+            success: function(employeeData) {
+              question.model.set('employees', employeeData);
+            }
+          })
+        },
         render: function() {
           $('#content').html( this.$el );
           this.$el.html( this.template() );
@@ -17,7 +39,9 @@ bridgelyApp.Views = bridgelyApp.Views || {};
           return this.el;
         },
         initialize: function() {
+          $("html, body").scrollTop(0);
           this.model.fetch();
+          this.getEmployees();
           // this.render();
           this.model.on('sync change', this.render, this);
         }
